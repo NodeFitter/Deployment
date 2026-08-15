6ecf055f4c62c947ae8dc0a5ccc7266c9f79d2f4
FireStoat3 <161834755+FireStoat3@users.noreply.github.com>
2026-08-15 13:33:11 +0200

    First implementation

--- initdb/init.sql
+++ initdb/init.sql
@@ -6,3 +6,8 @@ CREATE TABLE IF NOT EXISTS random_number (
 INSERT INTO random_number (id, number)
 VALUES (1, 42)
 ON DUPLICATE KEY UPDATE id = id;
+
+-- Config user to have metric access
+GRANT PROCESS, REPLICATION CLIENT, SELECT
+ON *.*
+TO 'test'@'%';
